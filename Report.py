6334b577ba6956689a0c3caf118bd2fc63343cb2
ahmadtara <ahmadtara.pratama@gmail.com>
2025-10-08 22:10:11 +0700

    Create Report.py

---
+++ Report.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def show():
+    st.title("📊 Laporan Penjualan")
+    st.write("Tampilkan laporan harian / bulanan.")
+    st.info("Grafik dan rekap bisa ditambahkan di sini.")
