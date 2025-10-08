9a03d3533e9cab06148392dda95909c9935ddd94
ahmadtara <ahmadtara.pratama@gmail.com>
2025-10-08 22:09:56 +0700

    Create Order.py

---
+++ Order.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def show():
+    st.title("🧾 Transaksi / Order")
+    st.write("Halaman kasir untuk input transaksi.")
+    st.success("Fitur: Tambah barang, hitung total, cetak nota.")
