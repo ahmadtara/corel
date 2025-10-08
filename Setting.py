f3f6520097bc794327e4c2c1c2a10f19697475e5
ahmadtara <ahmadtara.pratama@gmail.com>
2025-10-08 22:39:08 +0700

    Update Setting.py

--- Setting.py
+++ Setting.py
@@ -17,21 +17,23 @@ def load_config():
 
 def save_config(cfg):
     with open(CONFIG_FILE, "w") as f:
-        json.dump(cfg, f, indent=2)
+        json.dump(cfg, f, indent=4)
 
 def show():
     st.title("⚙️ Pengaturan Toko")
-
     cfg = load_config()
-    nama = st.text_input("Nama Toko", value=cfg["nama_toko"])
-    alamat = st.text_area("Alamat Toko", value=cfg["alamat"])
-    footer = st.text_area("Footer Nota", value=cfg["footer_nota"])
-    template = st.text_area("Template Pesan WhatsApp", value=cfg["template_wa"])
 
-    if st.button("💾 Simpan"):
-        cfg["nama_toko"] = nama
-        cfg["alamat"] = alamat
-        cfg["footer_nota"] = footer
-        cfg["template_wa"] = template
-        save_config(cfg)
-        st.success("✅ Pengaturan disimpan.")
+    nama_toko = st.text_input("Nama Toko", cfg["nama_toko"])
+    alamat = st.text_area("Alamat", cfg["alamat"])
+    footer = st.text_area("Teks Footer Nota", cfg["footer_nota"])
+    template_wa = st.text_area("Template Pesan WhatsApp", cfg["template_wa"])
+
+    if st.button("💾 Simpan Pengaturan"):
+        new_cfg = {
+            "nama_toko": nama_toko,
+            "alamat": alamat,
+            "footer_nota": footer,
+            "template_wa": template_wa
+        }
+        save_config(new_cfg)
+        st.success("✅ Pengaturan berhasil disimpan.")
